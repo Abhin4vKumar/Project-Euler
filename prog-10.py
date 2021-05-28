c6c53e8a50f19b33c4e5cab3b2259d61ece96f9d
Abhinav <abhinavkumar1312004@gmail.com>
2021-05-28 23:32:00 +0530

    fixed 10 .. made more efficient

--- prog-10.py
+++ prog-10.py
@@ -1,19 +1,15 @@
 # The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
-#
 # Find the sum of all the primes below two million.
-
-
+import math
 def check_prime_for_odd(n):
-    if n ==2 or n ==3:
-        return True
-    elif n < 2:
+    if n == 1:
         return False
-    for i in range(3,n//2 + 1):
-        if n%i ==0:
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        if n % i == 0:
             return False
     return True
-
-sum = 0
+# prime is only checked for odd numbers... hence 2 is not checked....
+sum = 2
 prime = 1
 while prime < 2000000:
     if check_prime_for_odd(prime):
