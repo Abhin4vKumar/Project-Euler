60c92f67408dd08f3e89c65b1b0f07c0ab193680
Abhinav <abhinavkumar1312004@gmail.com>
2021-05-30 16:53:33 +0530

    Made more simpler

--- prog-1.py
+++ prog-1.py
@@ -1,24 +1,12 @@
-def funcn(n,d):
-    continue_ = False
-    if isinstance(d,tuple) and isinstance(n,int):
-        continue_ = True
+def funcn(n):
+    if isinstance(n,int):
+        pass
     else:
-        print('n == class int , d == class tuple')
-    multiples = []
-    sum_ = 0
-    code = 'if '
-    count = 0
-    for i in d:
-        if count == 0:
-            extra_code = ''
-        else:
-            extra_code = 'or '
-        count+=1
-        code += extra_code + f'i%{i} == 0 '
-    code += ':\n\tmultiples.append(i)'
-    if continue_:
-        for i in range(n):
-            exec(code)
-    sum_ = sum(multiples)
-    print(sum_)
-funcn(1000,(3,5))
+        print('Unsupported Data types')
+        return None
+    sum = 0
+    for i in range(n):
+        if i%3 == 0 or i%5 == 0:
+            sum += i
+    print(sum)
+funcn(1000)
